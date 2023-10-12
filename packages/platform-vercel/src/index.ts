6bdcc9f9b8262ab321e7d149ce3c34f6f43331fa
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-12 17:07:55 +0900

    refactor: change interface

--- packages/platform-vercel/src/index.ts
+++ packages/platform-vercel/src/index.ts
@@ -1,26 +1,11 @@
 import { handle } from "hono/vercel";
 
-import type { Dairi } from "@dairi/core";
+import type { DairiPlatformMiddleware } from "@dairi/core";
 
-let handler: ReturnType<typeof handle> | null = null;
-
-const vercel = {
-  get: (app: Dairi) => {
-    if (handler) {
-      return handler;
-    }
-
-    handler = handle(app.run());
-    return handler;
-  },
-  post: (app: Dairi) => {
-    if (handler) {
-      return handler;
-    }
-
-    handler = handle(app.run());
-    return handler;
-  },
-};
+const vercel = (): DairiPlatformMiddleware => ({
+  name: "vercel-platform",
+  type: "platform",
+  getHandler: (server) => handle(server),
+});
 
 export { vercel };
